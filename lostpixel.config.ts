aca6d848b33fa3bda5630f51c98f2fb281a1aadd
d-ivashchuk <dimaiv@me.com>
2022-08-14 15:58:07 +0200

    fix lp config

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  pageShots: {
+    pages: [{ path: "/app", name: "app", id: "app" }],
+    pageUrl: "http://172.17.0.1:3000",
+  },
+  generateOnly: true,
+  failOnDifference: true,
+};
